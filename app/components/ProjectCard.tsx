dfe6e6bd9fc0872e174f3489a3a19e4626f98ac1
tejasksaindane <tejassaindane99@gmail.com>
2024-03-25 16:09:37 +0530

    some error checking

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -28,16 +28,16 @@ const ProjectCard = ({
         <img src={image} style={imageStyle} alt="Image" />
         <div className="absolute inset-0 flex justify-end m-3 ">
           {/* <a href="https://github.com/tejasksaindane/Form-Craft-"> */}
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src="./assets/github.png"
-                alt="github"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src="./assets/github.png"
+              alt="github"
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
           {/* </a> */}
         </div>
         <div className="p-[20px] mt-[-30px]">
@@ -51,11 +51,7 @@ const ProjectCard = ({
 
           {/* <div className="bg-red-400 mt-[10px] cursor-text"> */}
           {/* <Link href={site} passHref> */}
-          <a
-            href="https://github.com/tejasksaindane/Form-Craft-"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={site} target="_blank" rel="noopener noreferrer">
             <ExternalLink />
           </a>
           {/* </Link> */}
